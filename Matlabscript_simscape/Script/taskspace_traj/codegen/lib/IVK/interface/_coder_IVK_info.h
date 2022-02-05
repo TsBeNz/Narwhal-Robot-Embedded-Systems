cb972bb64fe5142443e78b2f2806c771978d1803
TsBeNz <42747938+TsBeNz@users.noreply.github.com>
2022-02-05 14:04:44 +0700

    Update Control
    - Control (Position Velocity)
    - Kalman filter (STM32,Matlab)
    - Encoder (AS5047U)

---
+++ Matlabscript_simscape/Script/taskspace_traj/codegen/lib/IVK/interface/_coder_IVK_info.h
@@ -0,0 +1,33 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ * File: _coder_IVK_info.h
+ *
+ * MATLAB Coder version            : 5.3
+ * C/C++ source code generated on  : 03-Feb-2022 12:17:12
+ */
+
+#ifndef _CODER_IVK_INFO_H
+#define _CODER_IVK_INFO_H
+
+/* Include Files */
+#include "mex.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Function Declarations */
+MEXFUNCTION_LINKAGE mxArray *emlrtMexFcnProperties(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+/*
+ * File trailer for _coder_IVK_info.h
+ *
+ * [EOF]
+ */
