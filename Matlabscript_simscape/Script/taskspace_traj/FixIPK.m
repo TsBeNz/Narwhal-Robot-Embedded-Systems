909f1840dd5574aacf9785aa28cd22de1d570b38
KarnMatas <56544166+KarnMatas@users.noreply.github.com>
2022-02-09 17:31:34 +0700

    update iknarwhale5 ultimatecorrect

--- Matlabscript_simscape/Script/taskspace_traj/FixIPK.m
+++ Matlabscript_simscape/Script/taskspace_traj/FixIPK.m
@@ -11,11 +11,12 @@ l3= 268.23;
 % l2 = 380.00;
 % l3= 268.23;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-TaskVar = [ (l2+l1) 0 (h1+h2-l3) -pi pi]% position(x,y,z) and pitch ,yaw
+% TaskVar = [ (l2+l1) 0 (h1+h2-l3) -pi pi]% position(x,y,z) and pitch ,yaw
 % TaskVar = [ (154.68) 0 (330.43) -pi pi]
 %(65.4+416) 0 ((180+310-150)) 0 0               (20+380) 0 ((251+380-285))
-P = TaskVar(1:3)'
-rbar = [0 TaskVar(4:5)]
+% P = TaskVar(1:3)'
+% rbar = [0 TaskVar(4:5)]
+chi = [(l1+l2) 0 (h1+h2-l3) 0]
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 DH =[0   0   h1   0;
     l1 pi/2  0  pi/2;
@@ -24,14 +25,15 @@ DH =[0   0   h1   0;
     0  pi/2  0    0];
 
 % gammabar = [1 1 1];
-gammabar = [1 1 1 1];
-[qbar] = IKnawhale2(P,rbar,gammabar,h1,h2,l1,l2,l3,DH)
+gammabar = [1 1 -1];
+% [qbar] = IKnawhale2(P,rbar,gammabar,h1,h2,l1,l2,l3,DH)
 % [qbar] = IKnarwhale3(P,rbar,gammabar,h1,h2,l1,l2,l3)
-test = [0;
-    0.8900;
-    1.2696;
-   -2.1596;
-    3.1416]
+[qbar] = IKnarwhale5(chi,gammabar,h1,h2,l1,l2,l3)
+% test = [0;
+%     0.8900;
+%     1.2696;
+%    -2.1596;
+%     3.1416]
 [H1,H2,H3,H4,H5,He] = FKnawhale(qbar,l3,DH)  
 % f = eul2rotm([],'ZYZ')
 karn= qbar
---
+++ Matlabscript_simscape/Script/taskspace_traj/testIKRR/RRIK.m
@@ -0,0 +1,7 @@
+function [q1,q2] = RRIK(x,y,l1,l2,gamma)
+    c2 = (x^2+y^2-l1^2-l2^2)/(2*l1*l2)
+    s2 = gamma*sqrt(1-c2^2)
+    q1 = (atan2(y,x)-atan2(l2*s2,l1+l2*c2))-(pi/2) 
+    q2 = atan2(s2,c2)+(pi/2)
+end
+
